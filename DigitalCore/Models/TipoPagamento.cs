9d812b59e1bdb68cb20e68ca1f2c560bb05bb8fd
Virgilio Quintino <estt8786@ipt.pt>
2020-04-26 17:25:01 +0100

    Criação das Classes do Modelo que representam as tabelas na BD

---
+++ DigitalCore/Models/TipoPagamento.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigitalCore.Models
+{
+    /// <summary>
+    /// Classe representa a tabela dos "TipoPagamento" na BD
+    /// </summary>
+    public class TipoPagamento
+    {
+        /// <summary>
+        /// PK da tabela
+        /// </summary>
+        [Key] // Anotação que força este atributo a ser PK. Mas, não seria necessário, pq o atributo chama-se "ID"
+        public int ID { set; get; }
+
+        /// <summary>
+        /// Descrição/identificação do Tipo de pagamento
+        /// </summary>
+        public string Descricao { get; set; }
+    }
+}
