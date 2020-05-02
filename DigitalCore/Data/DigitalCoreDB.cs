fad744a1acf24f8e9dcc66194f4481eae6f5c824
Filipe Branco <estt6967@ipt.pt>
2020-05-02 10:45:51 +0100

    Adicionar tabelas à BD

--- DigitalCore/Data/DigitalCoreDB.cs
+++ DigitalCore/Data/DigitalCoreDB.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DigitalCore.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,19 @@ namespace DigitalCore.Data
     public class DigitalCoreDB : DbContext
     {
         public DigitalCoreDB(DbContextOptions<DigitalCoreDB> options) : base(options) { }
+
+
+
+
+
+        // adicionar as 'tabelas' à Base de Dados
+        public virtual DbSet<Cliente> Cliente { get; set; }
+        public virtual DbSet<Imagem> Imagem { get; set; }
+        public virtual DbSet<TipoProduto> TipoProduto { get; set; }
+        public virtual DbSet<Caracteristicas> Caracteristicas { get; set; }
+        public virtual DbSet<Produto> Produto { get; set; }
+        public virtual DbSet<CaracteristicasProduto> CaracteristicasProduto { get; set; }
+        public virtual DbSet<Compra> Compra { get; set; }
+
     }
 }
