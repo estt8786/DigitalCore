7aa5d49d6818e943c649b16bb4dac76524cd6886
Filipe Branco <estt6967@ipt.pt>
2020-05-02 10:28:58 +0100

    Novo branch, e criação da base de dados

---
+++ DigitalCore/Data/DigitalCoreDB.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigitalCore.Data
+{
+    public class DigitalCoreDB : DbContext
+    {
+        public DigitalCoreDB(DbContextOptions<DigitalCoreDB> options) : base(options) { }
+    }
+}
--- DigitalCore/Startup.cs
+++ DigitalCore/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DigitalCore.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -20,11 +22,30 @@ namespace DigitalCore
 
         public IConfiguration Configuration { get; }
 
+
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            //****************************************************************************
+            // especificação do 'tipo' e 'localização' da BD
+            services.AddDbContext<DigitalCoreDB>(options =>
+               options.UseSqlServer(
+                   Configuration.GetConnectionString("ConnectionDB")));
+            //****************************************************************************
         }
+
+
+
+
+
+
+
+
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
